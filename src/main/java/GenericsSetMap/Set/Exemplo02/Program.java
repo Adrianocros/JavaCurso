8af1458f171bfec127c374fcd43851e270ac6d8d
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-02 23:28:30 -0300

    LinkedHashSet

---
+++ src/main/java/GenericsSetMap/Set/Exemplo02/Program.java
@@ -0,0 +1,22 @@
+package GenericsSetMap.Set.Exemplo02;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("Tv");
+        set.add("Tablet");
+        set.add("Notebook");
+
+       set.remove("Tablet");
+
+        for (String p : set){
+            System.out.println(p);
+        }
+
+    }
+}
+
