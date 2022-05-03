bcaf3720454024196111ebaace305b686b5e5625
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-02 23:43:40 -0300

    diferença'

--- src/main/java/GenericsSetMap/Set/Exemplo03Conjunto/Program.java
+++ src/main/java/GenericsSetMap/Set/Exemplo03Conjunto/Program.java
@@ -6,8 +6,8 @@ import java.util.TreeSet;
 
 public class Program {
     public static void main(String[] args) {
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,3,4,5,6,7,8,9,10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(2,5,6,7,8,9,10));
+        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,3,4,5,6,8,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
 
         //Union
         Set<Integer> c = new TreeSet<>(a);
@@ -18,6 +18,11 @@ public class Program {
         Set<Integer> d = new TreeSet<>(a);
         d.retainAll(b);
         System.out.println(d);
+
+        //diferença
+        Set<Integer> e = new TreeSet<>(a);
+        e.removeAll(b);
+        System.out.println(e);
 
     }
 }
