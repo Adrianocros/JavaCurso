9d88a6bdbb09c91642a5ad8be9420155b1b1f200
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-02 23:37:08 -0300

    intersecção

--- src/main/java/GenericsSetMap/Set/Exemplo03Conjunto/Program.java
+++ src/main/java/GenericsSetMap/Set/Exemplo03Conjunto/Program.java
@@ -7,11 +7,17 @@ import java.util.TreeSet;
 public class Program {
     public static void main(String[] args) {
         Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,3,4,5,6,7,8,9,10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
+        Set<Integer> b = new TreeSet<>(Arrays.asList(2,5,6,7,8,9,10));
 
         //Union
         Set<Integer> c = new TreeSet<>(a);
         c.addAll(b);
         System.out.println(c);
+
+        //intersecção
+        Set<Integer> d = new TreeSet<>(a);
+        d.retainAll(b);
+        System.out.println(d);
+
     }
 }
