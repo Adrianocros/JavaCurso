8ceba374ec9c3bcdc25423d3aa4507ed29c0a90b
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-02 22:21:29 -0300

    Correção

--- src/main/java/GenericsSetMap/GenericsExemploTres/Program.java
+++ src/main/java/GenericsSetMap/GenericsExemploTres/Program.java
@@ -6,15 +6,13 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        List<Integer> myList = Arrays.asList(5,2,3,4);
+        List<Integer> myList = Arrays.asList(5, 2, 3, 4);
         printList(myList);
         System.out.println();
 
         List<String> myLists = Arrays.asList("Maria", "Marcos", "Vania");
         printList(myLists);
-
     }
-
     private static void printList(List<?> list) {
         for(Object obj : list){
             System.out.println(obj);
