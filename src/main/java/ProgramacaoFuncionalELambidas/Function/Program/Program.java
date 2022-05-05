2a9adbc7b04cd30d421de4fa56ccf4d5b3e060bd
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-04 23:29:48 -0300

    Lambda declarada

--- src/main/java/ProgramacaoFuncionalELambidas/Function/Program/Program.java
+++ src/main/java/ProgramacaoFuncionalELambidas/Function/Program/Program.java
@@ -3,8 +3,10 @@ package ProgramacaoFuncionalELambidas.Function.Program;
 import ProgramacaoFuncionalELambidas.Function.Entities.Cliente;
 import ProgramacaoFuncionalELambidas.Function.util.UpperCaseName;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -15,8 +17,10 @@ public class Program {
         clientes.add(new Cliente("Tamara",35));
         clientes.add(new Cliente("Tamires",5));
 
+        Function<Cliente, String> cli = c -> c.getNome().toUpperCase();
+
         //Funcao map aplica uma função a cada elemento da strem gerando uma nova strem com elementos transformados
-        List<String> names = clientes.stream().map(Cliente :: NonstaticUpCaseName ).collect(Collectors.toList());
+        List<String> names = clientes.stream().map(cli).collect(Collectors.toList());
 
         names.forEach(System.out :: println);
 
